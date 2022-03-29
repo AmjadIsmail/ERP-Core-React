417729f412ab5d01616759139668d577f1f6ed4f
AmjadIsmail <66602030+AmjadIsmail@users.noreply.github.com>
2022-03-29 13:14:35 +0100

    month setup and gazate work

---
+++ ERP/Models/Gazatte.cs
@@ -0,0 +1,12 @@
+﻿namespace ERP.Models
+{
+    public class Gazatte
+    {
+
+        public int GazatID { get; set; }
+        public string GazatteName { get; set; }
+        public string GazatteDate { get; set; }
+        public int GtypeID { get; set; }
+        public string GtypeName { get; set; }
+    }
+}
---
+++ ERP/Models/LeaveClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ERP.Models
+{
+    public class LeaveClass
+    {
+        public int LeaveClassId { get; set; }
+        public string LeaveClassName { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedOn { get; set; }
+    }
+}
