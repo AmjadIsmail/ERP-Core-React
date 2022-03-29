274b3d54e47cdcb44e02445ba18522c8ad30e32e
= <=>
2022-03-29 07:44:59 +0100

    commit 2

--- ERP/Controllers/MonthSetupController.cs
+++ ERP/Controllers/MonthSetupController.cs
@@ -1,18 +1,18 @@
-﻿using ERP.Models;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using ERP.Models;
 
 namespace ERP.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GazatteController : ControllerBase
+    public class MonthSetupController : ControllerBase
     {
         private readonly IConfiguration _configuration;
-        public GazatteController(IConfiguration configuration)
+        public MonthSetupController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
@@ -20,8 +20,8 @@ namespace ERP.Controllers
         [HttpGet]
         public JsonResult Get()
         {
-            
-            string query = @"sp_GetGazatte";
+
+            string query = @"sp_GetMonthName";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("ERPAppCon");
             SqlDataReader myReader;
@@ -43,10 +43,10 @@ namespace ERP.Controllers
         }
 
         [HttpPost]
-        public JsonResult Post(Gazatte val)
+        public JsonResult Post(MonthSetup val)
         {
             //string query = @"insert into dbo.Departments (DeptName) values ('" + dep.DeptName + @"')";
-            string query = @"sp_postGazatte";
+            string query = @"sp_PostMonthName";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("ERPAppCon");
             SqlDataReader myReader;
@@ -56,9 +56,10 @@ namespace ERP.Controllers
                 using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
                     myCommand.CommandType = CommandType.StoredProcedure;
-                    myCommand.Parameters.AddWithValue("@GazatteName", val.GazatteName);
-                    myCommand.Parameters.AddWithValue("@GazatteDate", val.GazatteDate.Split('-')[2] + "-" + val.GazatteDate.Split('-')[1] + "-" + val.GazatteDate.Split('-')[0]); 
-                    myCommand.Parameters.AddWithValue("@Gtypeid", val.GtypeID);
+                    myCommand.Parameters.AddWithValue("@MonthName", val.MonthName);
+                    myCommand.Parameters.AddWithValue("@FromDate", val.FromDate.Split('-')[2] + "-" + val.FromDate.Split('-')[1] + "-" + val.FromDate.Split('-')[0]);
+                    myCommand.Parameters.AddWithValue("@ToDate", val.ToDate.Split('-')[2] + "-" + val.ToDate.Split('-')[1] + "-" + val.ToDate.Split('-')[0]);
+                    myCommand.Parameters.AddWithValue("@IsActive", val.IsActive);
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
@@ -70,10 +71,10 @@ namespace ERP.Controllers
         }
 
         [HttpPut]
-        public JsonResult Put(Gazatte val)
+        public JsonResult Put(MonthSetup val)
         {
-            
-            string query = @"sp_putGazatte";
+
+            string query = @"sp_PutMonthName";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("ERPAppCon");
             SqlDataReader myReader;
@@ -83,10 +84,11 @@ namespace ERP.Controllers
                 using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
                     myCommand.CommandType = CommandType.StoredProcedure;
-                    myCommand.Parameters.AddWithValue("@GazatteId", val.GazatID);
-                    myCommand.Parameters.AddWithValue("@GazatteName", val.GazatteName);
-                    myCommand.Parameters.AddWithValue("@GazatteDate", val.GazatteDate.Split('-')[2] + "-" + val.GazatteDate.Split('-')[1] + "-" + val.GazatteDate.Split('-')[0]);
-                    myCommand.Parameters.AddWithValue("@Gtypeid", val.GtypeID);
+                    myCommand.Parameters.AddWithValue("@AutoId ", val.AutoID);
+                    myCommand.Parameters.AddWithValue("@MonthName", val.MonthName);
+                    myCommand.Parameters.AddWithValue("@FromDate", val.FromDate.Split('-')[2] + "-" + val.FromDate.Split('-')[1] + "-" + val.FromDate.Split('-')[0]);
+                    myCommand.Parameters.AddWithValue("@ToDate", val.ToDate.Split('-')[2] + "-" + val.ToDate.Split('-')[1] + "-" + val.ToDate.Split('-')[0]);
+                    myCommand.Parameters.AddWithValue("@IsActive", val.IsActive);
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
 
@@ -102,7 +104,7 @@ namespace ERP.Controllers
         public JsonResult Delete(int id)
         {
             //string query = @"delete from dbo.Departments where Deptid  = '" + id + @"'";
-            string query = "sp_DeleteGazatte";
+            string query = "sp_DeleteMonthSetup";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("ERPAppCon");
             SqlDataReader myReader;
@@ -123,27 +125,6 @@ namespace ERP.Controllers
             return new JsonResult("Deleted Successfully.");
         }
 
-        [Route("GetAllGazatteTypes")]
-        public JsonResult GetAllGazatteTypes()
-        {
-            string query = @"sp_Getgazattetype";
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("ERPAppCon");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader); ;
-
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-
-            return new JsonResult(table);
-        }
+      
     }
 }
---
+++ ERP/Models/MonthSetup.cs
@@ -0,0 +1,11 @@
+﻿namespace ERP.Models
+{
+    public class MonthSetup
+    {
+        public int AutoID { get; set; }
+        public string MonthName { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
